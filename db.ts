1d83bdd9a481a00689112a74b1dc0b97ea5758b1
stepa <gershuni.stepan@gmail.com>
2023-04-12 17:25:44 +0100

    test

--- db.ts
+++ db.ts
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv';
-dotenv.config()
 
 console.log('SUPABASE: ' + process.env.SUPABASE_URL + ' pwd ' + process.env.SUPABASE_API_KEY)
 const supabaseUrl: string = process.env.SUPABASE_URL as string;
 const supabaseApiKey: string = process.env.SUPABASE_API_KEY as string;
 
-export const db = createClient("https://iolaelmcstjuiddlhwym.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvbGFlbG1jc3RqdWlkZGxod3ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjIwNDIsImV4cCI6MTk5Njc5ODA0Mn0.qqGSnRm2QvPGVKNg4CM_OAN3WYrl4GffFAGFkbgk08E");
+export const db = createClient(supabaseUrl, supabaseApiKey);
