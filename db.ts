8e75fb05c8bc6de464c252c4c8b74e0396e89e4f
stepa <gershuni.stepan@gmail.com>
2023-04-12 13:38:39 +0100

    asdasd

--- db.ts
+++ db.ts
@@ -6,4 +6,4 @@ console.log('SUPABASE: ' + process.env.SUPABASE_URL + ' pwd ' + process.env.SUPA
 const supabaseUrl: string = process.env.SUPABASE_URL as string;
 const supabaseApiKey: string = process.env.SUPABASE_API_KEY as string;
 
-export const db = createClient(supabaseUrl, supabaseApiKey);
+export const db = createClient("https://iolaelmcstjuiddlhwym.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvbGFlbG1jc3RqdWlkZGxod3ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjIwNDIsImV4cCI6MTk5Njc5ODA0Mn0.qqGSnRm2QvPGVKNg4CM_OAN3WYrl4GffFAGFkbgk08E");
