7ed0c61d1226d61235064e32efbd487a73d12868
stepa <gershuni.stepan@gmail.com>
2023-04-12 13:09:58 +0100

    db type fix

--- db.ts
+++ db.ts
@@ -2,7 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 dotenv.config()
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseApiKey = process.env.SUPABASE_API_KEY || '';
+console.log('SUPABASE: ' + process.env.SUPABASE_URL)
+const supabaseUrl: string = process.env.SUPABASE_URL as string;
+const supabaseApiKey: string = process.env.SUPABASE_API_KEY as string;
 
 export const db = createClient(supabaseUrl, supabaseApiKey);
